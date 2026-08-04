e21ec4084f37f2de745fe60188d5ae9fac645fbe
giakhang94 <ngk.khang94@gmail.com>
2026-08-04 15:54:29 +0700

    feat/#23 the remaining code, no reafactor, no updated features

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Patch, UseGuards } from '@nestjs/common';
+import { NotificationService } from './notification.service.js';
+import { JwtAuthGuard } from '../auth/guards/Jwt-auth.guard.js';
+import { GetUser } from '../auth/decorators/GetUser.Decorator.js';
+import type { JwtUser } from '../auth/interfaces/jwt-user.interface.js';
 
 @Controller('notification')
-export class NotificationController {}
+export class NotificationController {
+  constructor(private readonly notificationService: NotificationService) {}
+
+  @Get('/:id')
+  @UseGuards(JwtAuthGuard)
+  async getNotificationByUserId(@GetUser() user: JwtUser) {
+    return this.notificationService.getNotifications(Number(user.id));
+  }
+
+  @Patch('/mark-as-read/:id')
+  @UseGuards(JwtAuthGuard)
+  async markAsReadById(
+    @Param('id') notificationId: string,
+    @GetUser() user: JwtUser,
+  ) {
+    return this.notificationService.markAsRead(
+      Number(user.id),
+      Number(notificationId),
+    );
+  }
+}
